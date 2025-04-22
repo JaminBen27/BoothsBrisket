4fe9f6d98a67e286796f83aff5a12aba544f85e6
JaminBen27 <benecollett@gmail.com>
2025-04-22 10:23:35 -0500

    can click to move pieces(very buggy)

--- main.cpp
+++ main.cpp
@@ -17,69 +17,62 @@ using namespace std;
 #include "BearGame.h"
 
 int main() {
+    cout << "Debug: Program starting..." << endl;
 
     SDL_Plotter g(ROWS, COLUMNS);
+    cout << "Debug: SDL_Plotter initialized" << endl;
 
     BearGame game(g);
-    int x,y;
+    int x, y;
+    bool tokenSelected = false;
 
+    while(!g.getQuit()) {
+        // Handle mouse click (triggered on mouse button up)
+        if(g.mouseClick()) {
+            point click = g.getMouseClick(); // dequeue to prevent spamming
+            x = click.x;
+            y = click.y;
+            cout << "Mouse clicked at (" << x << ", " << y << ")" << endl;
 
-    while(!g.getQuit()){
-
-
-        if(g.kbhit()){
-            switch(g.getKey()){
-
-            }
-        }
-
-        if(g.getMouseDown(x,y)){
-            game.checkForClickOnToken(Point(x,y));
-        }
-
-        if(g.getMouseUp(x,y)){
-            if(game.isOver() && game.getPause()){
+            if(game.isOver() && game.getPause()) {
+                cout << "Debug: Game over, resetting" << endl;
                 game.setGameOver(false);
                 g.clear();
                 game = BearGame(g);
-            }
-            else{
-                game.updateTokenLocation(Point(x,y), g);
-                if(!game.isTigersTurn() && game.tigerWins()){
-                    game.displayFile(Point(150,450),"Tiger.bmp.txt",g);
-                    game.setGameOver(true);
-                    game.setPause(true);
+            } else {
+                if(!tokenSelected) {
+                    game.checkForClickOnToken(Point(x,y));
+                    tokenSelected = true;
+                } else {
+                    game.updateTokenLocation(Point(x,y), g);
+                    tokenSelected = false;
+
+                    if(!game.isTigersTurn() && game.tigerWins()) {
+                        cout << "Debug: Tiger wins!" << endl;
+                        game.displayFile(Point(150,450), "Tiger.bmp.txt", g);
+                        game.setGameOver(true);
+                        game.setPause(true);
+                    } else if(game.isTigersTurn() && game.manWins()) {
+                        cout << "Debug: Man wins!" << endl;
+                        game.displayFile(Point(210,450), "manwins.bmp.txt", g);
+                        game.setGameOver(true);
+                        game.setPause(true);
+                    }
                 }
-
-                else if(game.isTigersTurn() && game.manWins()){
-                    game.displayFile(Point(210,450),"manwins.bmp.txt",g);
-                    game.setGameOver(true);
-                    game.setPause(true);
-                }
             }
-
-
         }
 
-        if(g.getMouseMotion(x,y)){
-
+        // Only track motion if a token is already selected
+        if(g.getMouseMotion(x, y) && tokenSelected) {
             game.checkAttachedToken(Point(x,y), g);
         }
 
-        if(game.isTigersTurn()){
-            //game.randomMove(g); //Move Tiger
-        }
-        else{
-            //game.randomMove(g); //Move Man
-        }
-
-
-
-
-
-
         game.draw(g);
         g.update();
+
+        //cout.flush();  // flush console output
     }
+
+    cout << "Debug: Program ending..." << endl;
     return 0;
 }
